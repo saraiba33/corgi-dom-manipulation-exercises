1ebfebd02cb60f5f5120ba1fa12419fcd134d624
Sara Ibarra <saraiba33@gmail.com>
2022-03-31 08:03:00 -0600

    add ul & li

---
+++ exercises/12/index.js
@@ -0,0 +1,6 @@
+const queens = document.querySelector(".queens")
+const ul = document.createElement("ul")
+const li = document.createElement("li")
+queens.append(ul)
+li.textContent = "Kiki"
+ul.append(li)
