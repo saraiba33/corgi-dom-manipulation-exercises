5380ff58c33ef58a5823a183fc73ec36fb871f4e
Sara Ibarra <saraiba33@gmail.com>
2022-03-31 11:44:40 -0600

    log all src

---
+++ exercises/23/index.js
@@ -0,0 +1,5 @@
+const image1 = document.querySelector('[src="corgi.jpg"]');
+const image2 = document.querySelector('[src="evil-corgi.jpg"]');
+const image3 = document.querySelector('[src="cute-corgi.jpg"]');
+
+console.log(image1, image2, image3)
